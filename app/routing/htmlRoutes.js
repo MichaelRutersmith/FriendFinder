9409d2e4aa71af924579a1bafd67ae32528a831f
MichaelRutersmith <46669151+MichaelRutersmith@users.noreply.github.com>
2019-03-04 21:23:15 -0600

    healp

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -14,4 +14,8 @@ module.exports = function(app) {
   app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
   });
+  app.get("/favicon.ico", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+  
 }
